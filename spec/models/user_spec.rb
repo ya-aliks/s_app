878c9c1f6bd1b1dba2b809e8a6b1227e94641009
ya-aliks <ya-aliks@yandex.ru>
2015-01-26 11:45:26 +0300

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,6 +63,16 @@ describe User do
     it { should_not be_valid }
     end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
