2110c88c5bf8d4d19528826fcdf4c52954af2776
ya-aliks <ya-aliks@yandex.ru>
2015-01-27 14:46:25 +0300

    RE-Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -29,7 +29,7 @@ describe "Static pages" do
   end
     
   describe "Contact page" do
-    before { visit about_path }
+    before { visit contact_path }
 
   	it { should have_content 'Contact' } 
   	it { should have_title(full_title('Contact')) }
