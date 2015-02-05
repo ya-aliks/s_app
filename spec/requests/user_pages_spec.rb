055e9c07b27573e74d52fbe0c529242e01a1996e
ya-aliks <ya-aliks@yandex.ru>
2015-02-05 09:23:19 +0300

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,7 +52,8 @@ describe "signup page" do
       describe "after saving the user" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
-
+        
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
